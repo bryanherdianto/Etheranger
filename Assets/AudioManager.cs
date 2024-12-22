0d11f4c8b9dc68a14d992e74be64ad973027c6c6
Jeremy Wijanarko Mulyono <159864200+jrmywm@users.noreply.github.com>
2024-12-22 23:34:20 +0700

    Update AudioManager.cs
    fixed bgm script (biar gak double)

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -4,19 +4,29 @@ using UnityEngine.Scripting;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource backgroundMusic;
-
     public AudioClip background;
-    
+
+    private static AudioManager instance;
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
-    }
-    private void Start()
-    {
-        backgroundMusic.clip = background;
-        backgroundMusic.Play();
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    
+    private void Start()
+    {
+        if (instance == this)
+        {
+            backgroundMusic.clip = background;
+            backgroundMusic.Play();
+        }
+    }
 }
